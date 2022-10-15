a5d5909eebe94513edf54c6e8b18394b07a97252
pooja12git <pkadam2266@gmail.com>
2022-10-15 14:25:59 +0530

    second commit

---
+++ src/com/testproject/Testclass.java
@@ -0,0 +1,12 @@
+package com.testproject;
+
+public class Testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Quize project");
+
+	}
+
+}
